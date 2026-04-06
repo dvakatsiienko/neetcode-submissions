5b8c9f168431177a321f8b1891603b57c275ab08
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2026-04-06 16:55:19 +0300

    Add: string-encode-and-decode - submission-13

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-13.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        return strs.map(str => {
+            return `${str.length}#${str}`;
+        }).join('');
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(istr) {
+        const strs = [];
+        let i = 0;
+        let len = '';
+
+        while (i < istr.length) {
+            const sym = istr[i];
+
+            if (typeof !isNaN(parseInt(sym))) {
+                len = len + sym;
+            }
+
+            if (sym === '#') {
+                const strLen = Number.parseInt(len);
+                const dstr = istr.substr(i + 1, strLen);
+
+                strs.push(dstr);
+                i = i + strLen;
+                len = '';
+            }
+
+            i++;
+        }
+
+        return strs;
+    }
+}
